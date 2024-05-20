2156ed98a9897b9ef7ca73e03d7a8d90f011f824
Anton <you@example.com>
2024-05-20 03:23:23 +0300

    add responsive

--- CarSite/src/components/Layout.jsx
+++ CarSite/src/components/Layout.jsx
@@ -1,11 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "./Header";
 import { Box } from "@mui/material";
 
 export default function Layout() {
+  const { pathname } = useLocation();
   return (
     <Box>
-      <Box sx={{ position: "relative", maxWidth: "100%" }}>
+      <Box
+        sx={{
+          position: `${pathname === "/" ? "relative" : "static"}`,
+          maxWidth: "100%",
+        }}
+      >
         <Header />
         <Outlet />
       </Box>
