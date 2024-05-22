bd52f113860afbf520093541fceeabf236c4200b
Anton <you@example.com>
2024-05-22 20:20:35 +0300

    delete position "absolute"

--- CarSite/src/components/Layout.jsx
+++ CarSite/src/components/Layout.jsx
@@ -1,14 +1,12 @@
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import { Box } from "@mui/material";
 
 export default function Layout() {
-  const { pathname } = useLocation();
   return (
     <Box>
       <Box
         sx={{
-          position: `${pathname === "/" ? "relative" : "static"}`,
           maxWidth: "100%",
         }}
       >
