cf2f3fbdc053b16411ad2da8c595f20cb1a5dba7
Anton <you@example.com>
2024-05-22 20:17:19 +0300

    add filter and cards components

--- CarSite/src/AllModelsPage.jsx
+++ CarSite/src/AllModelsPage.jsx
@@ -1,9 +1,16 @@
+import Grid from "@mui/material/Unstable_Grid2/Grid2";
 import Filter from "./components/allModelsComponents/Filter";
+import Cards from "./components/allModelsComponents/Cards";
 
 export default function AllModelsPage() {
   return (
-    <>
+    <Grid
+      container
+      columns={{ xs: 4, sm: 8, md: 12, lg: 16, xl: 18 }}
+      justifyContent="center"
+    >
       <Filter />
-    </>
+      <Cards />
+    </Grid>
   );
 }
