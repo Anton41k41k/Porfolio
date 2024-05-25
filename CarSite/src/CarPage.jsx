5244931e86390271b68a0d0bf93211dc62fa2088
Anton <you@example.com>
2024-05-25 14:09:07 +0300

    add new components

--- CarSite/src/CarPage.jsx
+++ CarSite/src/CarPage.jsx
@@ -2,6 +2,11 @@ import Grid from "@mui/material/Unstable_Grid2/Grid2";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Presentation from "./components/carPageComponents/Presentation";
+import Design from "./components/carPageComponents/Desing";
+import { CircularProgress } from "@mui/material";
+import Features from "./components/carPageComponents/Features";
+import Characteristics from "./components/carPageComponents/Characteristics";
+import CallbackPopup from "./components/carPageComponents/CallbackPopup";
 
 export default function CarPage() {
   const { carId } = useParams();
@@ -20,13 +25,35 @@ export default function CarPage() {
       .then((result) => setInformation(result));
   }, [carId]);
   return (
-    <Grid container columns={{ xs: 4, sm: 8, md: 12, lg: 16, xl: 18 }}>
-      {!!information && (
+    <Grid
+      container
+      columns={{ xs: 4, sm: 8, md: 12, lg: 16, xl: 18 }}
+      justifyContent="center"
+    >
+      {information ? (
         <Presentation
           backdrop_path={information.backdrop_path}
           model={information.model}
           overview={information.overview}
           make={information.make}
+          year={information.year}
+          total={information.total}
+        />
+      ) : (
+        <CircularProgress sx={{ mt: "15%" }} />
+      )}
+      {information && (
+        <Features features={information.features} model={information.model} />
+      )}
+      {information && (
+        <Characteristics characteristics={information.characteristics} />
+      )}
+      {information && <Design design={information.design} />}
+      {information && (
+        <CallbackPopup
+          make={information.make}
+          model={information.model}
+          total={information.total}
         />
       )}
     </Grid>
